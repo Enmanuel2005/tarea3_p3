c5724c2843be77320d913dab27ead51cc7062003
Enmanuel2005 <111095171+Enmanuel2005@users.noreply.github.com>
2024-07-29 21:01:44 -0400

    the function already work with all numbers that you want.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hola Mundo!')
+function sum(...numbers) {
+    const result = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    console.log(result);
+
+    return result;
+}
+
+sum(5, 19);          
+sum(1, 2, 3, 4, 5);   
